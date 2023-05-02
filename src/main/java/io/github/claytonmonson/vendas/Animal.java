b7c123eccc339f16c0c8643a14f4e216d1bddd9d
clayton_monson <clayton_monson@sicredi.com.br>
2023-05-02 10:45:52 -0300

    commit inicial

---
+++ src/main/java/io/github/claytonmonson/vendas/Animal.java
@@ -0,0 +1,5 @@
+package io.github.claytonmonson.vendas;
+
+public interface Animal {
+    void fazerBarulho();
+}
---
+++ src/main/java/io/github/claytonmonson/vendas/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.claytonmonson.vendas.repository;
+
+import io.github.claytonmonson.vendas.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void persistir(Cliente cliente) {
+        //acessa a base e salva o cliente
+    }
+}
