578442178af39eb1c47537c593836b4a5c6a0411
Clayton <clayton_monson@hotmail.com>
2023-05-13 18:38:20 -0300

    Carregando os pedidos por cliente

--- src/main/java/io/github/claytonmonson/vendas/VendasApplication.java
+++ src/main/java/io/github/claytonmonson/vendas/VendasApplication.java
@@ -33,9 +33,12 @@ public class VendasApplication {
 
 			pedidos.save(p);
 
-			Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-			System.out.println(cliente);
-			System.out.println(cliente.getPedidos());
+			pedidos.findByCliente(fulano).forEach(System.out::println);
+
+
+//			Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
+//			System.out.println(cliente);
+//			System.out.println(cliente.getPedidos());
 
 //			List<Cliente> result = clientes.encontrarPorNome("Douglas");
 //			result.forEach(System.out::println);
--- src/main/java/io/github/claytonmonson/vendas/domain/repository/Pedidos.java
+++ src/main/java/io/github/claytonmonson/vendas/domain/repository/Pedidos.java
@@ -1,7 +1,11 @@
 package io.github.claytonmonson.vendas.domain.repository;
 
+import io.github.claytonmonson.vendas.domain.entity.Cliente;
 import io.github.claytonmonson.vendas.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
+    List<Pedido> findByCliente(Cliente cliente);
 }
