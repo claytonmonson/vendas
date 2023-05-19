a46ecc8bbc0bc8060ee461077f2cf8c3cc735022
Clayton <clayton_monson@hotmail.com>
2023-05-19 17:47:54 -0300

    Atualizado repository clientes

--- src/main/java/io/github/claytonmonson/vendas/domain/repository/Clientes.java
+++ src/main/java/io/github/claytonmonson/vendas/domain/repository/Clientes.java
@@ -2,24 +2,6 @@ package io.github.claytonmonson.vendas.domain.repository;
 
 import io.github.claytonmonson.vendas.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface Clientes extends JpaRepository<Cliente, Integer> {
-
-    @Query(value = " Select c From Cliente c Where c.nome like '%:nome%' ")
-    List<Cliente> encontrarPorNome(@Param("nome") String nome);
-
-    @Query(value = " delete from Cliente c Where c.nome = :nome")
-    @Modifying
-    void deleteByNome(String nome);
-
-    List<Cliente> findByNomeLike(String nome);
-    boolean existsByNome(String nome);
-
-    @Query(" select c from Cliente c left join fetch c.pedidos where c.id = :id ")
-    Cliente findClienteFetchPedidos( Integer id );
 }
